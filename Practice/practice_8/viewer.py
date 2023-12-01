c505ebe551f4b3aa7fcf069fb124822e1feab432
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2023-12-01 12:23:51 +0300

    Update viewer.py

--- Practice/practice_8/viewer.py
+++ Practice/practice_8/viewer.py
@@ -2,18 +2,10 @@ from english import *
 
 
 def show():
-    list = get_words()
-    print(list)
-    normal_list = (normal_words(list))
-    print(normal_list)
-    d = count_words(normal_list)
-    print(d)
-    sorted_list = (sort_words(d))
-    print(sorted_list)
-    dictionary = translate_words(sorted_list)
-    with open('dictionary.csv', 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Исходное слово', 'Перевод', 'Количество упоминаний'])
-        for i in dictionary:
-            writer.writerow(i)
-    print('Словарь успешно создан!')
+    dict = translate_l(sort_list(make_dict(normal_w(get_w()))))
+    with open('data.csv', 'w', newline='', encoding='utf-8') as file:
+        wr = csv.writer(file)
+        wr.writerow(['Исходное слово', 'Перевод', 'Количество упоминаний'])
+        for i in dict:
+            wr.writerow(i)
+    print('Словарь создан')
