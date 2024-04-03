2f8cfc392fcbb43dbecf4de3e4167164d7c15e52
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2024-04-03 11:55:13 +0300

    Update kr1.py

--- Lapchinskaya/kr1.py
+++ Lapchinskaya/kr1.py
@@ -2,4 +2,5 @@ import math
 
 for x in [i / 10 for i in range(20, 52, 2)]:
     print(f'x = {"{:.3f}".format(x)} f(x) = {"{:.3f}".format(3 * x * math.sin(x) ** 2 + math.tan(2 * x))}')
+print('=====')
 print('Артём Беспечалов 2023-ФГиИБ-ПИ-1б')
