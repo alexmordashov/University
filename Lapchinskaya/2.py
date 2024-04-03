2fd9d68fa32e1f2b0e8d0514e81b3fe16b22934e
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2024-04-03 11:53:36 +0300

    Update 2.py

--- Lapchinskaya/2.py
+++ Lapchinskaya/2.py
@@ -5,4 +5,5 @@ alpha = 2 * math.pi * random.random()
 print(alpha)
 print('z1 =', "{:.2f}".format((math.cos(alpha) + math.sin(alpha)) / (math.cos(alpha) - math.sin(alpha))))
 print('z2 =', "{:.2f}".format(math.tan(2 * alpha) + 1 / math.cos(2 * alpha)))
-
+print()
+print('Артём Беспечалов 2023-ФГиИБ-ПИ-1б')
