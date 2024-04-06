4e12d691db8ab6970e38f43cf2ccc68d2dd5bdf8
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2024-04-06 11:38:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 '''n = int(int(input()) ** 0.5) + 1
 c = 1
 for i in range(1, n):
-    c += 1
+    c += 2
     print(i ** 2)
 print(c)'''
 
@@ -28,7 +28,7 @@ c = 3
 while n != 0:
     if n >= max1:
         max2, max1 = max1, n
-        c += 1
+        c += 2
     elif n >= max2:
         max2 = n
         c += 2
@@ -75,18 +75,18 @@ for i in range(1, len(arr) - 1):
     if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
         ind.append(i)
         c += 1
-    c += 3
+    c += 6
 arr.clear()
-c += 2
+c += 1
 for i in range(len(ind) - 1):
     arr.append(abs(ind[i] - ind[i + 1]))
-    c += 3
+    c += 5
 if arr:
     print(min(arr))
     c += 1
 else:
     print(0)
-c += 2
+c += 1
 
 print(f'Количество операций: {c + 1}')'''
 
@@ -95,18 +95,22 @@ print(f'Количество операций: {c + 1}')'''
 '''n = int(input('Введите длину: '))
 c = 1
 arr = [int(input()) for i in range(n)]
-c += n * 2
+c += n * 3
 for i in range(0, n - 1, 2):
     arr[i], arr[i + 1] = arr[i + 1], arr[i]
-    c += 3
+    c += 4
 print(arr)
 
 print(f'Количество операций: {c}')'''
+
+'''for i in range(1, 9):
+    print(i, 3 + (i // 2) * 5 + (i - 2) * ((i - 1) % 2) + (i % 2) * (i - 1))'''
 
 # 7
 
 '''n = int(input('Введите длину: '))
 arr = [int(input())]
+c = 2
 for i in range(n - 1):
     a = int(input())
     if a < arr[i]:
@@ -115,26 +119,35 @@ for i in range(n - 1):
             b = int(input(f'Введите число больше или равно {arr[i]}: '))
             if b >= arr[i]:
                 arr.append(b)
+                c += 1
                 break
+            c += 3
     else:
         arr.append(a)
+        c += 1
+    c += 4
 print(arr)
 kol = 1
+c += 1
 for i in range(1, len(arr)):
     if arr[i] > arr[i - 1]:
         kol += 1
-print(kol)'''
+        c += 1
+    c += 4
+print(kol)
+
+print(c)'''
 
 # 8
 
 '''n = int(input('Введите длину: '))
 c = 1
 arr = [int(input()) for i in range(n)]
-c += n * 2
+c += n * 3
 for i in range(n):
     if arr[i] not in arr[i + 1:] and arr[i] not in arr[:i]:
         print(arr[i])
-    c += 3
+    c += 5
 print(arr)
 
 print(c)'''
