4c8f46b3d523b253a4575793ab9afe9b422cbd31
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2024-05-02 14:36:11 +0300

    Update game.py

--- Maksimova/Project_1/game.py
+++ Maksimova/Project_1/game.py
@@ -1,68 +1,90 @@
-# Файл Game
-def clear_file():
+from student import Student
+import os
+
+'''def game():
+    while True:
+        with open("data.txt") as f:
+            arr = f.read().splitlines()
+        student = Student(arr[0], arr[1], arr[2], arr[3], arr[4])
+        print(student)
+        otvet = input('Что вы хотите сделать?\n1 - Поработать\n2 - Развлечься\n3 - Поесть\n4 - Выйти\n')
+        if otvet == "1":
+            pass
+        elif otvet == "2":
+            pass
+        elif otvet == "3":
+            pass
+        else:
+            break
+        print(student)'''
+
+
+def clear_level():
     f = open('test.txt', 'w+')
     f.seek(0)
     f.close()
 
+
+def read_level():
+    with open("data.txt") as f:
+        arr = f.read().splitlines()
+    return arr
+
+
+def write_level(arr):
+    with open("data.txt", 'w+') as f:
+        f.write('\n'.join(arr))
+
+
 def game():
+    arr = ['100', '1000', '100', '0', '100', '100']
     while True:
-        otvet = input('Что вы хотите сделать?')
-        if otvet == 'Начать новую игру':
-            clear_file
-            student = new Student('параметры')
+        otvet = input('Что вы хотите сделать?\n1 - Начать новую игру\n2 - Продолжить игру\n3 - Выйти')
+        if otvet == '1':
+            write_level(arr)
+            student = Student(arr[0], arr[1], arr[2], arr[3], arr[4])
             game1(student)
-        if otvet == 'Продолжить игру':
-            f = open('test.txt', 'r')
-            if f.isempty():
-                print('У вас нет начатых игр')
-                student = new Student('параметры')
+        if otvet == '2':
+            if os.stat("data.txt").st_size == 0:
+                print('У вас нет начатой игры. Начнем новую игру!')
+                write_level(arr)
+                student = Student(arr[0], arr[1], arr[2], arr[3], arr[4])
             else:
-                student = new Student('параметры из текстового файла')
+                arr = read_level()
+                student = Student(arr[0], arr[1], arr[2], arr[3], arr[4])
             game1(student)
             break
         else:
             break
-def game1():
+
+
+def game1(student):
     while True:
-        print(student) # выводим характеристики студента и дату
-        otvet = input('Что будем делать?')
-        if otvet == 'Работать':
+        print(student)  # выводим характеристики студента и дату
+        otvet = input('Что будем делать?\n1 - Работать\n2 - Есть\n3 - Кайфовать\n4 - Учиться\n5 - Начать следующий день\n6 - Выйти из игры')
+        if otvet == '1':
             student.work()
-        if otvet == 'Есть':
+        if otvet == '2':
             student.food()
         if otvet == 'Кайфовать':
             student.entertainment()
-        if otvet == 'Учиться':
+        if otvet == '4':
             student.study()
-        if otvet == 'Начать следующий день':
+        if otvet == '5':
             student.next_day()
-        if otvet == 'Выйти из игры':
+        if otvet == '6':
             break
         if 'рейтинг в университете' == 0:
             print('вас исключили, вы проиграли')
-            clear_file()
+            clear_level()
             break
         if 'здоровье' == 0:
             print('вы умерли')
-            clear_file()
+            clear_level()
             break
         if 'настроение' == 0:
             print('вы впали в депрессию')
-            clear_file()
+            clear_level()
         if 'энергия' == 0:
             print('вы устали')
             student.next_day()
-            
-# Файл Student
-def next_day():
-    # изменить текстовый файл(энергия +100, здоровье -10), рейтинг в университете +(0 или -10 если не посетил занятия))
-    continue
-def food():
-    # выводим возможную еду из файла csv
-    # считываем выбор игрока и применяем его
-def work():
-    # выводим возможную работу из файла csv
-    # считываем выбор игрока и применяем его
-def entertainment():
-    # выводим возможные развлечения из файла csv
-    # считываем выбор игрока и применяем его
