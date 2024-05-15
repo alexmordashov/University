4c78d26f57dcb4cc19bd0afd1c8ed97995d3c72f
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2024-05-15 20:24:25 +0300

    Update student.py

--- Maksimova/Project_1/student.py
+++ Maksimova/Project_1/student.py
@@ -1,35 +1,47 @@
 class Student:
-    def __init__(self, health, money, mood, iq, energy, rating):
+    def __init__(self, health, money, mood, iq, energy, rating, date):
         self.health = health
         self.money = money
         self.mood = mood
         self.iq = iq
         self.energy = energy
         self.rating = rating
+        self.date = date
 
     def __str__(self):
         return f"Здоровье: {self.health}\nНастроение: {self.mood}\nИнтеллект: {self.iq}\nРейтинг в университете:{self.rating}\nЭнергия: {self.energy}\nДеньги: {self.money}"
 
     def next_day(self):
         # изменить текстовый файл(энергия +100, здоровье -10), рейтинг в университете +(0 или -10 если не посетил занятия))
+        self.health -= 100
+        self.energy = 100
+        #self.rating = ?
+        #self.mood = ?
+        self.date += 1
         with open("data.txt", 'w+') as f:
             f.write('\n'.join())
         pass
 
     def food(self):
-        # выводим возможную еду из файла csv
-        # считываем выбор игрока и применяем его
-        pass
+        with open('food.txt', encoding='utf-8') as f:
+            for i in f.readlines():
+                print(i[:-1])
+        otvet = input('Что будем делать?')
 
     def work(self):
-        # выводим возможную работу из файла csv
-        # считываем выбор игрока и применяем его
-        pass
+        with open('work.txt', encoding='utf-8') as f:
+            for i in f.readlines():
+                print(i[:-1])
 
     def entertainment(self):
-        # выводим возможные развлечения из файла csv
-        # считываем выбор игрока и применяем его
-        pass
+        with open('entertainment.txt.txt', encoding='utf-8') as f:
+            for i in f.readlines():
+                print(i[:-1])
+
+    def study(self):
+        with open('study.txt', encoding='utf-8') as f:
+            for i in f.readlines():
+                print(i[:-1])
 
     '''def clear_level(self):
         f = open('test.txt', 'w+')
