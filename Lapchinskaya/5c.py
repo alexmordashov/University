e7f79be169aa99f798a90857f52d68259d7513ef
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2024-05-22 14:19:02 +0300

    Update 5c.py

--- Lapchinskaya/5c.py
+++ Lapchinskaya/5c.py
@@ -5,14 +5,16 @@ class Function:
     def __init__(self, x0, xn, hx):
         self.x0 = x0
         self.xn = xn
-        self.hx = hx
+        self.hx = str(hx)
 
 
     def func(self):
         kol = 0
         if '.' in self.hx:
             kol = len(self.hx.split('.')[1])
-        for x in [i / 10**kol for i in range(int(self.x0 * 10**kol), int((self.xn + float(self.hx)) * 10**kol), int(float(self.hx) * 10**kol))]:
+        for x in [i / 10**kol for i in range(int(self.x0 * 10**kol),
+                                             int((self.xn + float(self.hx)) * 10**kol),
+                                             int(float(self.hx) * 10**kol))]:
             if x <= -3 or x >= 6:
                 print(f'x = {"{:.3f}".format(x)} f(x) = {"{:.3f}".format(3)}')
             elif x > -3 and x < 0:
@@ -21,7 +23,7 @@ class Function:
                 print(f'x = {"{:.3f}".format(x)} f(x) = {"{:.3f}".format(x * 0.5)}')
 
 
-graf = Function(-10, 20, '0.05')
+graf = Function(-10, 20, 0.05)
 graf.func()
 print('=====')
 print('Артём Беспечалов 2023-ФГиИБ-ПИ-1б')
