7155fe3406f2f54902ba9015ff23678c44d8071e
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2024-06-05 12:02:25 +0300

    Update exceptions.py

--- Lapchinskaya/exceptions.py
+++ Lapchinskaya/exceptions.py
@@ -1,8 +1,7 @@
-import os
-
 try:
     with open(input('Введите имя файла: '), 'r') as f:
         print(''.join(f.readlines()))
 except FileNotFoundError as e:
     print('Файл не найден')
-print("Программу выполнил студент 2023-ФГиИБ-ПИ-1б Беспечалов Артём")
+print('=====')
+print('Артём Беспечалов 2023-ФГиИБ-ПИ-1б')
