d689085ccaf0303e774e884f90f42aec5d47d1e7
alexmordashov <144428481+alexmordashov@users.noreply.github.com>
2024-06-13 10:05:14 +0300

    Update classwork_with_files.py

--- Lapchinskaya/classwork_with_files.py
+++ Lapchinskaya/classwork_with_files.py
@@ -6,9 +6,9 @@ a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
 p = 1.6075
-print(f'a = {a}', f'b = {b}', f'c = {c}', sep='\n')
-print('Объём эллипсоида:', "{:.3f}".format(4 / 3 * math.pi * a * b * c))
-print('Площадь поверхности эллипсоида:',
-      "{:.3f}".format(((a ** p * b ** p + b ** p * c ** p + c ** p * a ** p) / 3) ** (1 / p) * 4 * math.pi))
-print('=====')
+f.close()
+with open("new_data.txt", 'w', encoding='utf-8') as f:
+    f.write(str("{:.3f}".format(4 / 3 * math.pi * a * b * c)) + '\n')
+    f.write(str("{:.3f}".format(((a ** p * b ** p + b ** p * c ** p + c ** p * a ** p) / 3) ** (1 / p) * 4 * math.pi)))
+f.close()
 print('Артём Беспечалов 2023-ФГиИБ-ПИ-1б')
